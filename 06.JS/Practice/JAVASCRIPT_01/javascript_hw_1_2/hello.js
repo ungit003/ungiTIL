b8634f6f62b066842d0e3c4f27628faa9e4a6c12
ungit003 <ungit003@gmail.com>
2024-10-30 09:21:07 +0900

    241030_practice_js01

---
+++ 06.JS/Practice/JAVASCRIPT_01/javascript_hw_1_2/hello.js
@@ -0,0 +1,15 @@
+﻿const a = document.querySelector("img");
+a.setAttribute("src", "profile.jpg");
+a.setAttribute("alt", "잘생긴 얼굴");
+
+const b = document.querySelector("#job");
+b.textContent = "의적";
+
+const c = document.querySelector("#experience");
+c.textContent = "서당";
+
+const d = document.querySelector("#email");
+d.textContent = "hgd@korea";
+
+const e = document.querySelector("#phone");
+e.textContent = "010-1234-1234";
